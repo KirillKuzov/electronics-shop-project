8be4dce879e4746810fa85b7e7b863ba4bae91a7
К <hitandrun444@yandex.ru>
2024-01-28 19:31:21 +0300

    add file

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,32 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+
+from src.item import Item
+
+
+def item1():
+    return Item("Смартфон", 10000, 20)
+
+
+def phone1():
+    return Phone("iPhone 14", 120_000, 5, 2)
+
+
+def test__repr__(item1):
+    assert item1.__repr__() == "Item('Смартфон', 10000, 20)"
+
+
+def test_str(item1):
+    assert item1.__str__() == "Смартфон"
+
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 200000
+
+
+def test_apply_discount(item1):
+    Item.pay_rate = 1.0
+    item1.apply_discount()
+    assert item1.price == 10000
+
