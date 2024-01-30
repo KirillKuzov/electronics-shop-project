c948a3e9a2394b7fb40ab746c70c14e8d7ef9dfb
К <hitandrun444@yandex.ru>
2024-01-30 16:27:55 +0300

    add file

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -17,6 +20,48 @@ class Item:
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        """
+        Геттер приватного атрибута класса
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Сеттер приватного атрибута класса
+        """
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[0:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, path_to_csv="../src/items.csv"):
+        """
+        Класс метод для создания объектов из данных файла. Использует перехват исключений.
+        """
+        cls.all.clear()
+        with open(path_to_csv, newline="", encoding="windows-1251") as csvfile:
+            reader = csv.DictReader(csvfile)
+            items_objs_lst = []
+            for row in reader:
+                if len(row) == 3:
+                    items_objs_lst.append(cls(name=row["name"], price=row["price"], quantity=row["quantity"]))
+        return items_objs_lst
+
+    @staticmethod
+    def string_to_number(args: str):
+        """
+        Статический метод, возвращающий число из числа-строки
+        """
+        if "." in args:
+            args = args.split(".")[0]
+            return int(args)
+        else:
+            return int(args)
 
     def calculate_total_price(self) -> float:
         """
