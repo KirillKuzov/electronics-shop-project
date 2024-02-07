90a1046a3e0a1e8cf26eb7335284dc41a150e558
К <hitandrun444@yandex.ru>
2024-02-07 18:41:18 +0300

    add file

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,15 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-
 import csv
 
 import pytest
 
 from src.item import Item
+from src.phone import Phone
 
 
 @pytest.fixture
 def item1():
     return Item("Смартфон", 10000, 20)
+
+
+@pytest.fixture
+def phone1():
+    return Phone("iPhone 14", 120_000, 5, 2)
 
 
 def test__repr__(item1):
@@ -28,6 +33,10 @@ def test_apply_discount(item1):
     Item.pay_rate = 1.0
     item1.apply_discount()
     assert item1.price == 10000
+
+
+# def test_private_name(item1):
+#     assert item1.name == item1._Item__name
 
 
 def test_setter_name(item1):
@@ -67,3 +76,11 @@ def test_string_to_number():
     assert Item.string_to_number("5") == 5
     assert Item.string_to_number("7.8") == 7
     assert Item.string_to_number("3.14") == 3
+
+
+def test_summ_quantity(item1, phone1):
+    assert phone1 + item1 == 25
+    assert item1 + phone1 == 25
+    with pytest.raises(ValueError):
+        assert phone1 + 10 == 15
+
